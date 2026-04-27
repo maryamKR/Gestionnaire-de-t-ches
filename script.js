9aec0b65250a1630de3abe00026903bf53a25ee2
maryam <mariem.kerrous@gmail.com>
2026-04-27 23:23:59 +0100

    create checkbox is working now

--- script.js
+++ script.js
@@ -38,6 +38,31 @@ btn_add.addEventListener("click", () => {
   const span = document.createElement("span")
   span.textContent = tache
 
+
+  /** Checkbox */
+  const svgMarkup = `
+<svg class="w-[15px] h-[12px] absolute top-[6px] left-[5px]" viewBox="0 0 10 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M9 0.75L3.49997 6L0.75 3.375" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`;
+  let span_svg = document.createElement("span")
+  span_svg.classList.add("hidden")
+  span_svg.innerHTML += svgMarkup
+  let label = document.createElement("label")
+  label.classList.add("relative")
+ 
+
+  let check = document.createElement("input")
+  check.type = "checkbox"
+  check.classList.add("check")
+  
+  
+  label.addEventListener("change", () => {
+    span_svg.classList.toggle("hidden")
+    span.classList.toggle("line-through")
+    span.classList.toggle("text-gray-500")
+  })
+
   const btn_remove =  document.createElement("button")
   const img_remove =  document.createElement("img")
   img_remove.src = "assets/cancel_40dp_E3E3E3_FILL0_wght400_GRAD0_opsz40.svg"
@@ -47,7 +72,9 @@ btn_add.addEventListener("click", () => {
   btn_remove.addEventListener("click", () => {
     li_list.remove()
   })
-
+   label.appendChild(span_svg)
+  label.appendChild(check)
+  li_list.appendChild(label)
   li_list.appendChild(span)
   li_list.appendChild(btn_remove)
 
